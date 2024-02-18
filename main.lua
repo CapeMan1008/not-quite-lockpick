97f7db798a1f85e870eeb292f52f509945688f1e
CapeMan <capeman1008@gmail.com>
2024-02-17 21:24:44 -0500

    First tests + important thing I forgot

--- main.lua
+++ main.lua
@@ -2,6 +2,22 @@ require "src.const"
 require "src.complexnumber"
 require "src.key"
 require "src.door"
+require "src.objects"
+
+function love.load()
+    ObjectList[1] = {
+        x = 16,
+        y = 16,
+        type = "key",
+        data = {
+            color = "orange",
+            type = "add",
+            active = true,
+            amount = CreateComplexNum(1),
+            reusable = false
+        }
+    }
+end
 
 function love.draw()
     for _, obj in ipairs(ObjectList) do
