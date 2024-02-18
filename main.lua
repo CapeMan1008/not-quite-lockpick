1545af23c05ab13de6c5a98667eff225370765a5
CapeMan <capeman1008@gmail.com>
2024-02-17 21:18:14 -0500

    Now objects should be drawn

--- main.lua
+++ main.lua
@@ -2,3 +2,9 @@ require "src.const"
 require "src.complexnumber"
 require "src.key"
 require "src.door"
+
+function love.draw()
+    for _, obj in ipairs(ObjectList) do
+        DrawObject(obj)
+    end
+end
