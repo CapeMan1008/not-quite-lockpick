c9b5b87c5ad402627acbac8903f71cedb753a559
CapeMan <capeman1008@gmail.com>
2024-08-28 17:02:01 -0400

    Add PlayerUpdate function

--- src/player.lua
+++ src/player.lua
@@ -16,3 +16,9 @@ function DrawPlayer()
     love.graphics.setColor(1,1,1)
     love.graphics.rectangle("fill", Player.x, Player.y, PLAYER_WIDTH, PLAYER_HEIGHT)
 end
+
+---@param dt number
+function UpdatePlayer(dt)
+    Player.y = Player.y + Player.velY * dt
+    Player.velY = Player.velY + PLAYER_GRAVITY * dt
+end
--- src/update.lua
+++ src/update.lua
@@ -1,5 +1,7 @@
 ---@param dt number
 function love.update(dt)
+    UpdatePlayer(dt)
+
     AnimationTimer = AnimationTimer + dt
 
     Palette.wild = {HSVtoRGB(AnimationTimer*12,1,1)}
