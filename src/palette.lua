11e2a005afa735348a4623bb474fa5adf214737a
CapeMan <capeman1008@gmail.com>
2024-08-28 15:36:36 -0400

    Implement rudimentary parsing of palette files (not to be confused with actual .pal files)

--- src/palette.lua
+++ src/palette.lua
@@ -17,3 +17,73 @@ Palette = {
     ice = {0.4,0.9,1},
     null = {0,0,0,0},
 }
+
+--TODO
+---@param filepath string
+function LoadPaletteFromFile(filepath)
+    local fileInfo = love.filesystem.getInfo(filepath, "file")
+
+    if not fileInfo then
+        return
+    end
+
+    local file = love.filesystem.newFile(filepath, "r")
+
+    if not file then
+        return
+    end
+
+    local lines = {}
+
+    for line in file:lines() do
+        lines[#lines+1] = line
+    end
+
+    ParsePaletteData(lines)
+end
+
+---@param lines string[]
+function ParsePaletteData(lines)
+    ---@type string
+    local currentColor = nil
+
+    for _, line in ipairs(lines) do
+        ---@type string,string
+        local cmd, params = string.match(line, "^%s*(%S*)%s*(.*)")
+
+        if cmd == "color" then
+            ---@type string
+            local color = string.match(params, "^([%w_]+)")
+
+            if color then
+                currentColor = color
+            end
+        end
+
+        if cmd == "base" then
+            local r,g,b,a = string.match(params, "^(b?[%d%.]*)%s*(b?[%d%.]*)%s*(b?[%d%.]*)%s*(b?[%d%.]*)")
+
+            ---@param str string
+            ---@return number?
+            local function annoyingConversion(str)
+                if string.find(str, "^b") then
+                    local num = tonumber(string.sub(str, 2))
+
+                    if num then
+                        return num / 255
+                    end
+
+                    return num
+                end
+
+                return tonumber(str)
+            end
+
+            r,g,b,a = annoyingConversion(r),annoyingConversion(g),annoyingConversion(b),annoyingConversion(a)
+
+            if currentColor then
+                Palette[currentColor] = {r or 0, g or 0, b or 0, a or 1}
+            end
+        end
+    end
+end
