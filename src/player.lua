88aa8c4bfd5e2046977708201178f907918dbea7
CapeMan <capeman1008@gmail.com>
2024-08-28 16:35:21 -0400

    Create player table

---
+++ src/player.lua
@@ -0,0 +1,13 @@
+---@class Player
+---@field x number
+---@field y number
+---@field velY number
+---@field jumps integer
+---@field coyoteTime number
+Player = {
+    x = 0,
+    y = 0,
+    velY = 0,
+    jumps = 0,
+    coyoteTime = 0,
+}
