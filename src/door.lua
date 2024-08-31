fca6da75dd4c8e00b7bf06d32fb2227fcb175416
CapeMan <capeman1008@gmail.com>
2024-08-31 16:06:30 -0400

    Make CheckMasterKey return true on copying a door

--- src/door.lua
+++ src/door.lua
@@ -48,7 +48,7 @@
 ---@param use_master boolean? If true, checks if you can use a master key on the door before preforming normal checks.
 ---@param imaginary boolean? If true, tries to open an imaginary copy of the door instead of a real copy.
 ---@param no_open boolean? If true, returns a value without opening the door.
----@return boolean can_open If you can open the door.
+---@return boolean can_open If you can open the door (also returns true if you copy a door).
 ---@return ComplexNumber? cost The number of keys spent to open the door.
 ---@return ComplexNumber? wild_cost The number of wildcard keys spent to open the door.
 function TryOpenDoor(door, use_master, imaginary, no_open)
@@ -132,7 +132,7 @@ end
 ---@param door Door The door to try opening.
 ---@param imaginary boolean? If true, tries to open an imaginary copy of the door instead of a real copy.
 ---@param no_open boolean? If true, returns a value without opening the door.
----@return boolean can_open If you can open the door.
+---@return boolean can_open If you can open the door (also returns true if you copy a door).
 function CheckMasterKey(door, imaginary, no_open)
     ---@type boolean
     local master_immune = DoesDoorHaveColor(door, "pure") or DoesDoorHaveColor(door, "master")
@@ -149,18 +149,12 @@ function CheckMasterKey(door, imaginary, no_open)
 
             if door.copies.real <= 0 then
                 CopyDoor(door, false, true)
-                return false
             else
                 OpenDoor(door, false, false, true)
-                return true
-            end
-        else
-            if door.copies.imaginary <= 0 then
-                return false
-            else
-                return true
             end
         end
+
+        return true
     elseif KeyStates.master.count.imaginary > 0 and imaginary then
         if not no_open then
             if not KeyStates.master.star then
@@ -169,18 +163,12 @@ function CheckMasterKey(door, imaginary, no_open)
 
             if door.copies.imaginary <= 0 then
                 CopyDoor(door, true, true)
-                return false
             else
                 OpenDoor(door, true, false, true)
-                return true
-            end
-        else
-            if door.copies.real <= 0 then
-                return false
-            else
-                return true
             end
         end
+
+        return true
     elseif KeyStates.master.count.real < 0 and not imaginary then
         if not no_open then
             if not KeyStates.master.star then
@@ -189,18 +177,12 @@ function CheckMasterKey(door, imaginary, no_open)
 
             if door.copies.real < 0 then
                 OpenDoor(door, false, true, true)
-                return true
             else
                 CopyDoor(door, false, false)
-                return false
-            end
-        else
-            if door.copies.real < 0 then
-                return true
-            else
-                return false
             end
         end
+
+        return true
     elseif KeyStates.master.count.imaginary < 0 and imaginary then
         if not no_open then
             if not KeyStates.master.star then
@@ -209,18 +191,12 @@ function CheckMasterKey(door, imaginary, no_open)
 
             if door.copies.imaginary < 0 then
                 OpenDoor(door, true, true, true)
-                return true
             else
                 CopyDoor(door, true, false)
-                return false
-            end
-        else
-            if door.copies.imaginary < 0 then
-                return true
-            else
-                return false
             end
         end
+
+        return true
     end
 
     return false
